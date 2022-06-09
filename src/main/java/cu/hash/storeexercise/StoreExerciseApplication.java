2869e712bbbe04bd26cdf0d73c0d7928c9588fe4
humberto.cabrera <humbercabdom@gmail.com>
2022-06-09 09:23:24 -0400

    Uploading code

---
+++ src/main/java/cu/hash/storeexercise/StoreExerciseApplication.java
@@ -0,0 +1,13 @@
+package cu.hash.storeexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cu/hash/storeexercise/StoreExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package cu.hash.storeexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
